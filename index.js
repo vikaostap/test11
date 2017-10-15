4fd6cb2972bef65228c68dd40307c68cdfb9c1fc
vikaostap <vikaostap7@gmail.com>
2017-10-15 12:57:35 +0300

    Add factorial

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert("Hello!")
+function formSubmit (e) {
+	e.preventDefault();
+	var number = Number(e.target.num.value);
+	console.log('type:', typeof number);
+	console.log(number);
+	alert(factorial(number));
+}
+
+function factorial(num) {
+   var res = 1;
+   for(var i = 1; i < num; i++){
+   	res *= i;
+   }
+  return res;
+}
+
+document.getElementById('my-form')
+  .addEventListener('submit', formSubmit);
